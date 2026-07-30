73a8da9cc02b4c2c638077bd6dec4dc6a15fce0c
midischwarz12 <midischwarz@proton.me>
2026-07-30 00:09:33 -0500

    ci: align angular workflows

--- flake.nix
+++ flake.nix
@@ -6,11 +6,33 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    angular-language-server = pkgs.angular-language-server;
   in {
     formatter.${system} = pkgs.alejandra;
-    packages.${system}.default = pkgs.angular-language-server;
+
+    packages = {
+      ${system}.default = angular-language-server;
+    };
+
+    checks = {
+      ${system} = {
+        package = angular-language-server;
+
+        format = pkgs.runCommand "angular-flake-format" {nativeBuildInputs = [pkgs.alejandra];} ''
+          alejandra --check ${./flake.nix}
+          touch $out
+        '';
+
+        smoke = pkgs.runCommand "angular-language-server-smoke" {nativeBuildInputs = [angular-language-server];} ''
+          command -v ngserver
+          ngserver --help
+          touch $out
+        '';
+      };
+    };
+
     devShells.${system}.default = pkgs.mkShell {
-      packages = [pkgs.angular-language-server pkgs.pnpm];
+      packages = [angular-language-server pkgs.pnpm];
     };
   };
 }
